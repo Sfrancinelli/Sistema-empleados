8f3e212ea3748ff43561684103018c2218a8f844
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2022-11-30 10:08:57 -0300

    Conectando app a base de datos

--- src/app.py
+++ src/app.py
@@ -5,5 +5,23 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'empleados'
+
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    sql = 'insert into empleados (nombre, correo, foto) values ("Juan", "juan@email.com", "fotodejuan.jpg");' 
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+
+    conn.commit()
+
+    return render_template('empleados/index.html')
+
 if __name__=='__main__':
     app.run(debug=True)
