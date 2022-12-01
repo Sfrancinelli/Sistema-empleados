613b6f8b5d23f8991d6d0a15865a5c1c66251fec
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2022-12-01 12:02:21 -0300

    Creé rutas /create y /store. Modifiqué /index

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -12,16 +12,39 @@ app.config['MYSQL_DATABASE_DB'] = 'empleados'
 
 mysql.init_app(app)
 
-@app.route('/')
+# Establece la ruta del navegador (URL). En el parametro se especifica la misma. Se accede a la misma mediante el método "GET". Como es el método por defecto, no se especifica como parámetro, pero sería como poner @app.route('/URL',methods = ["GET"])
+@app.route('/', methods = ["GET"])
 def index():
-    sql = 'insert into empleados (nombre, correo, foto) values ("Juan", "juan@email.com", "fotodejuan.jpg");' 
     conn = mysql.connect()
     cursor = conn.cursor()
+    sql = 'SELECT * FROM empleados;'
     cursor.execute(sql)
+
+    empleados = cursor.fetchall()
 
     conn.commit()
 
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', empleados = empleados)
+
+@app.route('/create', methods = ["GET"])
+def create():
+    
+    return render_template('empleados/create.html')
+
+@app.route('/store', methods = ["POST"])
+def store():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
+
+    sql = "INSERT INTO empleados (nombre, correo, foto) values (%s, %s, %s);"
+    datos = (_nombre, _correo, _foto.filename)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return redirect('/') # Es lo mismo que poner redirect('/index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
